7c2b2d17b14e94919e2e1ec6f90665703ab4bd3b
Vineetsh7 <xyz.meoutlook010@gmail.com>
2026-02-07 15:02:44 +0530

    Implemented encrypt/decrypt function

--- src/app/processes/process_management.cpp
+++ src/app/processes/process_management.cpp
@@ -7,16 +7,19 @@
 
 ProcessManagement::ProcessManagement() {}
 
-bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task) {
+bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task)
+{
     taskQueue.push(std::move(task));
     return true;
 }
 
-void ProcessManagement::executeTasks() {
-    while (!taskQueue.empty()) {
+void ProcessManagement::executeTasks()
+{
+    while (!taskQueue.empty())
+    {
         std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
         taskQueue.pop();
         std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
         executeCryption(taskToExecute->toString());
     }
-}
+} 
